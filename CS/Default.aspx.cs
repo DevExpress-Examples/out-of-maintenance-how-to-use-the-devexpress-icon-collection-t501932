45f4d067d481ad16d2f1dcddaa4cfab5c7f4d93c
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Default.aspx.cs
@@ -0,0 +1,28 @@
+﻿using DevExpress.Web;
+using DevExpress.Web.ASPxThemes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page {
+    protected override void OnInit(EventArgs e) {
+        base.OnInit(e);
+		ASPxButton button4 = new ASPxButton();        
+        button4.ID = "RuntimeButton";        
+        ph.Controls.Add(button4);
+        button4.CssClass = "button";
+        button4.Image.IconID = DevExpress.Web.ASPxThemes.IconID.NavigationHome32x32;
+        LiteralControl lc = new LiteralControl("<span>This ASPxButton is created at runtime in PlaceHolder</span>");
+        ph.Controls.Add(lc);
+
+    }
+
+    protected void Button2_Init(object sender, EventArgs e) {
+        Button btn = sender as Button;
+        btn.CssClass += " dxIcon_" + IconID.NavigationHome32x32;
+    }
+}
---
+++ VB/Default.aspx.vb
@@ -0,0 +1,30 @@
+﻿Imports DevExpress.Web
+Imports DevExpress.Web.ASPxThemes
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Reflection
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+
+Partial Public Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Overrides Sub OnInit(ByVal e As EventArgs)
+        MyBase.OnInit(e)
+        Dim button4 As New ASPxButton()
+        button4.ID = "RuntimeButton"
+        ph.Controls.Add(button4)
+        button4.CssClass = "button"
+        button4.Image.IconID = DevExpress.Web.ASPxThemes.IconID.NavigationHome32x32
+        Dim lc As New LiteralControl("<span>This ASPxButton is created at runtime in PlaceHolder</span>")
+        ph.Controls.Add(lc)
+
+    End Sub
+
+    Protected Sub Button2_Init(ByVal sender As Object, ByVal e As EventArgs)
+        Dim btn As Button = TryCast(sender, Button)
+        btn.CssClass &= " dxIcon_" & IconID.NavigationHome32x32
+    End Sub
+End Class
